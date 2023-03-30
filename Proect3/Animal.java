9b1b572a803aa0b76ec7db9beaf6cf37e476eeee
Olga-6 <olga.chichikin1968@mail.ru>
2023-03-30 23:50:37 +0300

    ver 3.1

--- Proect3/Animal.java
+++ Proect3/Animal.java
@@ -13,6 +13,26 @@ public abstract class Animal implements Action {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
     
 
 }
